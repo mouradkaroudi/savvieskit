b68d21dec039042aa85306c1c23a17502c0c2fef
unknown <mkaroudi@gmail.com>
2021-12-10 20:13:04 +0100

    general structure

--- src/components/SideNav/SideNavItem.jsx
+++ src/components/SideNav/SideNavItem.jsx
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 const SideNavItem = ({ 
-    className: customClassName, 
+    className: customClassName,
     children, 
     isActive,
     ...rest }) => {
--- src/components/Text/index.jsx
+++ src/components/Text/index.jsx
@@ -10,11 +10,13 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 const Text = ({
+    children,
+    customClasses,
     size,
-    as
+    as,
 }) => {
 
-
+    //React.createElement(as, children)
 
 }
 
